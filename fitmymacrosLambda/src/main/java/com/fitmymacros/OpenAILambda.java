784101b12142793c5c687f0ce5e9293a5fe7975e
Pablo <pabloskixd23@gmail.com>
2024-03-19 22:31:19 +0100

    commenting some constructor stuff

--- fitmymacrosLambda/src/main/java/com/fitmymacros/OpenAILambda.java
+++ fitmymacrosLambda/src/main/java/com/fitmymacros/OpenAILambda.java
@@ -38,13 +38,14 @@ public class OpenAILambda implements RequestHandler<Map<String, Object>, Object>
     private String OPENAI_MODEL;
     private Double MODEL_TEMPERATURE;
     private final String RESULT_TABLE_NAME = "FitMyMacros_OpenAI_Results";
-    private final DynamoDbClient dynamoDbClient;
+    private DynamoDbClient dynamoDbClient;
     private String URL = "https://api.openai.com/v1/chat/completions";
-    private ObjectMapper objectMapper;
-    private WebClient webClient;
+    // private ObjectMapper objectMapper;
+    // private WebClient webClient;
 
     public OpenAILambda() {
-        this.dynamoDbClient = DynamoDbClient.builder().region(Region.EU_WEST_3).build();
+        // this.dynamoDbClient =
+        // DynamoDbClient.builder().region(Region.EU_WEST_3).build();
         // this.objectMapper = new ObjectMapper();
         // this.webClient = WebClient.create();
     }
@@ -54,9 +55,12 @@ public class OpenAILambda implements RequestHandler<Map<String, Object>, Object>
         try {
             System.out.println("input: " + input);
             this.ssmClient = SsmClient.builder().region(Region.EU_WEST_3).build();
+            this.dynamoDbClient = DynamoDbClient.builder().region(Region.EU_WEST_3).build();
             this.OPENAI_AI_KEY = this.getOpenAIKeyFromParameterStore();
             this.OPENAI_MODEL = this.getOpenAIModelFromParameterStore();
             this.MODEL_TEMPERATURE = this.getTemperatureFromParameterStore();
+            ObjectMapper objectMapper = new ObjectMapper();
+            WebClient webClient = WebClient.create();
             Map<String, String> queryParams = this.extractQueryString(input);
             String opId = queryParams.get("opId").toString();
             String prompt = generatePrompt(queryParams);
